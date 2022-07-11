25ddc20df44bef1a79f6ab185022fb5e5ac885c3
Wahaj404 <wahajmaqsood20@gmail.com>
2022-07-11 21:40:37 +0500

    puzzle 2 complete

--- episode_1.py
+++ episode_1.py
@@ -1,3 +1,7 @@
+from collections import defaultdict
+from skspatial.objects import Plane, Points
+
+
 class Person:
     def __init__(self, lines: list[str]):
         self.name = lines[0].split(":")[1].strip()
@@ -18,11 +22,28 @@ def read_persons(fname: str) -> list[Person]:
     return [Person(lines[i : i + 14]) for i in range(0, len(lines), 14)]
 
 
+def read_galaxy(fname: str) -> list:
+    with open(fname, "r") as f:
+        return {
+            line[0].strip(): list(map(int, line[1].strip()[1:-1].split(",")))
+            for line in map(lambda line: line.split(":"), f)
+        }
+
+
 def puzzle1(persons: list[Person]) -> set[str]:
     return {p.id for p in persons if p.has_pico()}
+
+
+def puzzle2(persons: list[Person]) -> set[str]:
+    galaxy = read_galaxy("galaxy_map.txt")
+    plane = Plane.best_fit(Points(list(galaxy.values())))
+    outliers = {k for k, v in galaxy.items() if plane.distance_point(v) > 2}
+    return {p.id for p in persons if p.home_planet in outliers}
 
 
 if __name__ == "__main__":
     persons = read_persons("population.txt")
     p1 = puzzle1(persons)
     print(f"p1: {sum(map(int, p1))}")
+    p2 = puzzle2(persons)
+    print(f"p2: {sum(map(int, p2))}")
