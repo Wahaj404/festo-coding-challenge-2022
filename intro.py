62b37d9824bb14dbc221217dcc52ae816c32a280
Wahaj404 <wahajmaqsood20@gmail.com>
2022-07-11 20:17:31 +0500

    puzzle 1 done

---
+++ intro.py
@@ -0,0 +1,30 @@
+class Person:
+    name: str
+    id: str
+    access_key: int
+    hour: int
+    minute: int
+
+    def __init__(self, line):
+        parts = line.split(",")
+        self.name = parts[0].strip()
+        self.id = parts[1].strip()
+        self.access_key = int(parts[2].strip())
+        login_time = parts[3].strip().split(":")
+        self.hour = int(login_time[0])
+        self.minute = int(login_time[1])
+
+
+def read_persons() -> list[Person]:
+    with open("office_database.txt", "r") as f:
+        return [Person(line) for line in f]
+
+
+def puzzle1(persons: list[Person]) -> set[str]:
+    return {p.id for p in persons if "814" in p.id}
+
+
+if __name__ == "__main__":
+    persons = read_persons()
+    p1 = puzzle1(persons)
+    print(f"p1: {sum(map(int, p1))}")
