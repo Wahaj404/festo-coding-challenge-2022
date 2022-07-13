50f74a00560f2effeefd9d17c45438ad79355044
Wahaj404 <wahajmaqsood20@gmail.com>
2022-07-13 19:18:52 +0500

    puzzle 2 complete

--- episode_3.py
+++ episode_3.py
@@ -1,5 +1,5 @@
-from itertools import chain
-from copy import deepcopy
+from collections import defaultdict
+from queue import Queue
 
 
 class Person:
@@ -59,12 +59,50 @@ def read_persons(fname: str) -> list[Person]:
     return [Person(lines[i : i + 14]) for i in range(0, len(lines), 14)]
 
 
+def read_galaxy(fname: str) -> dict[str, list[int]]:
+    with open(fname, "r") as f:
+        return {
+            line[0].strip(): list(int(x) for x in line[1].strip()[1:-1].split(","))
+            for line in map(lambda line: line.split(":"), f)
+        }
+
+
+def dist(c1: tuple[float, float, float], c2: tuple[float, float, float]) -> float:
+    return sum((a - b) ** 2 for a, b in zip(c1, c2)) ** 0.5
+
+
 def puzzle1(persons: list[Person]) -> set[str]:
     seqs = ["pic", "opi", "cop", "ico"]
     return sorted({p.id for p in persons if p.has_pico(seqs)})
+
+
+def puzzle2(persons: list[Person]) -> set[str]:
+    signal_ranging = {"Venis": 2, "Cetung": 4, "Phoensa": 9}
+    galaxy = read_galaxy("galaxy_map.txt")
+    graph = defaultdict(list)
+    for p1, c1 in galaxy.items():
+        for p2, c2 in galaxy.items():
+            if dist(c1, c2) < 50:
+                graph[p1].append(p2)
+    running_set = set(galaxy.keys())
+    for planet, delay in signal_ranging.items():
+        q = Queue()
+        q.put(planet)
+        distances = {}
+        distances[planet] = 0
+        while not q.empty():
+            u = q.get()
+            for v in graph[u]:
+                if v not in distances:
+                    distances[v] = distances[u] + 1
+                    q.put(v)
+        running_set &= {p for p, d in distances.items() if d == delay}
+    return {p.id for p in persons if p.home_planet in running_set}
 
 
 if __name__ == "__main__":
     persons = read_persons("population.txt")
     p1 = puzzle1(persons)
     print(f"p1: {sum(map(int, p1))}")
+    p2 = puzzle2(persons)
+    print(f"p2: {sum(map(int, p2))}")
