56042953f3c57a36b7e6cbe0440cdd2ff4b36920
Wahaj Ameen <wahajmaqsood20@gmail.com>
2022-07-20 20:45:25 +0500

    puzzle 2 complete

--- final_episode.py
+++ final_episode.py
@@ -33,5 +33,30 @@ def puzzle1():
     return "-".join(map(str, sorted(clearable)))
 
 
+def puzzle2():
+    with open("bunker_gold.txt", "r") as f:
+        lines = f.readlines()
+    mx = float("-inf")
+    for i in range(0, len(lines), 4):
+        pname = lines[i]
+        bunkers = [
+            [int(bunker.strip()) for bunker in lines[i + j].strip().split(",")]
+            for j in (1, 2)
+        ]
+        cols = len(bunkers[0])
+        dp = [[0] * cols for _ in (0, 1)]
+        dp[0][0] = bunkers[0][0]
+        dp[1][0] = bunkers[1][0]
+        for i in range(1, cols):
+            dp[0][i] = max(bunkers[0][i] + dp[1][i - 1], dp[0][i - 1])
+            dp[1][i] = max(bunkers[1][i] + dp[0][i - 1], dp[1][i - 1])
+        res = max(dp[0][-1], dp[1][-1])
+        if mx < res:
+            mx = res
+            planet = pname.strip()
+    return f"{planet}{mx}"
+
+
 if __name__ == "__main__":
     print(f"p1: {puzzle1()}")
+    print(f"p2: {puzzle2()}")
